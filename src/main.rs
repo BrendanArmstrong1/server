409eae6bb0791c949398b9bc1d74bb724fc22835
Brendan Armstrong <Brendan.Armstrong@mail.com>
2022-04-05 09:38:00 -0400

    added sleep path with a duration to display the blocking nature of the single threaded process

--- src/main.rs
+++ src/main.rs
@@ -15,9 +15,13 @@ fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
     stream.read(&mut buffer).unwrap();
     let get: &[u8; 16] = b"GET / HTTP/1.1\r\n";
+    let sleep = b"GET /sleep HTTP/1.1\r\n";
 
     let (status_line, filename) = 
         if buffer.starts_with(get) {
+            ("HTTP/1.1 200 OK", "index.html")
+        } else if buffer.starts_with(sleep){
+            thread::sleep(Duration::from_secs(5));
             ("HTTP/1.1 200 OK", "index.html")
         } else {
             ("HTTP/1.1 404 NOT FOUND", "404.html")
